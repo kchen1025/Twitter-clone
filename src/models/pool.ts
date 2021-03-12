9a88b53a9c06d556765b3e66ae19fdf38d014344
Kevin Chen Personal <kchen1025@gmail.com>
2021-03-11 18:34:45 -0800

    boilerplate sql stuff

--- src/models/pool.ts
+++ src/models/pool.ts
@@ -1,11 +1,9 @@
-import * as dotenv from 'dotenv';
 import { Pool } from 'pg';
-
-dotenv.config();
+require('dotenv').config();
 
 // refresher on how to connect,
 // I use pg-cli for all direct database connections and elephantsql for easy instances setup
 // the password goes into your .pgpass file
 // the command for launching sql goes in your bash_profile and will look like this
 // pgcli -h <dbURL> -U <DBUSER> -d <DBNAME>'
-export const pool = new Pool({ connectionString: process.env.PROJ_CONNECTION_STRING });
+export const pool = new Pool({ connectionString: process.env.PROJ_CONNECTION_STRING_TWITTER });
